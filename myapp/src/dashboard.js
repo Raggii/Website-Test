0c04e105a8cf56afd307c732e5c6beda8830aafb
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2020-11-21 21:46:45 +1300

    0.0.0.0.0.0.0.4
    Added stuff

---
+++ myapp/src/dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+class dashboard extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+            <h1>Shopping List for </h1>
+        </div>
+      );
+    }
+  }
+
+
+
+ReactDOM.render(
+  <dashboard />,
+  document.getElementById('root')
+);
