94f6686cb109af495698b4f1d1998be636406d8c
SerBABA <dan2ronen@gmail.com>
2020-12-19 12:05:38 +1300

    Update 0.0.9

--- myapp/src/comps/navbar.js
+++ myapp/src/comps/navbar.js
@@ -5,42 +5,63 @@ function Navbar() {
   return (
     <>
       <nav>
-        <section className='item'>
+        <section className="item">
           <Link to="/">
             <img
               src="http://chasebovineservices.com/wp-content/uploads/2019/09/CBS-Logo.20190924-305x194.jpg"
               alt="Chase Bovine Services"
             />
-            <p>Dairy Nutrition and Veterinary Consulting</p>
           </Link>
-        
-        <ul className= 'navBarHeadings'>
-          <li >
-            <NavLink exact to="/" activeClassName="active-nav-link" className='btn'>
-              <h6>Home</h6>  
-            </NavLink>
-          </li>
-          <li >
-            <NavLink to="/about-me" activeClassName="active-nav-link" className='btn'>
-              <h6>About Me</h6>
-            </NavLink>
-          </li>
-          <li >
-            <NavLink to="/resources" activeClassName="active-nav-link" className='btn'>
-              <h6>Resources</h6>
-            </NavLink>
-          </li>
-          <li >
-            <NavLink to="/contact" activeClassName="active-nav-link" className='btn'>
-              <h6>Contact</h6>
-            </NavLink>
-          </li>
-          <li > 
-            <NavLink to="/login" activeClassName="active-nav-link" className='btn'>
-              <h6>login</h6>
-            </NavLink>
-          </li>
-        </ul>
+          <p>Dairy Nutrition and Veterinary Consulting</p>
+
+          <ul className="navBarHeadings">
+            <li>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active-nav-link"
+                className="btn"
+              >
+                <h6>Home</h6>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about-me"
+                activeClassName="active-nav-link"
+                className="btn"
+              >
+                <h6>About Me</h6>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/resources"
+                activeClassName="active-nav-link"
+                className="btn"
+              >
+                <h6>Resources</h6>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                activeClassName="active-nav-link"
+                className="btn"
+              >
+                <h6>Contact</h6>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/login"
+                activeClassName="active-nav-link"
+                className="btn"
+              >
+                <h6>login</h6>
+              </NavLink>
+            </li>
+          </ul>
         </section>
       </nav>
     </>
