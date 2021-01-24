d2eaecbd9e7dfbf9d049ee951d3a0f1c570d2114
Dan <dan2ronen@gmail.com>
2021-01-24 21:19:01 +1300

    about-me and resources
    - Fixed the CSS to work for 1980x1080

--- myapp/src/routes/resources.js
+++ myapp/src/routes/resources.js
@@ -6,12 +6,16 @@ import styles from "./../styles/resources.module.css";
 const Resources = () => {
   return (
     <>
-      <Link
-        to="/pregnancy-generation-preparation-for-success-20200416"
-        className={styles.link_item}
-      >
-        Improving Pregnancy Generation: Preparation for Success
-      </Link>
+      <div className={styles.resources_container}>
+        <div className={styles.resource_item_wrap}>
+          <Link
+            to="/pregnancy-generation-preparation-for-success-20200416"
+            className={styles.resource_item}
+          >
+            Improving Pregnancy Generation: Preparation for Success
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
