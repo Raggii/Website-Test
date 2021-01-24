2b7cdbc35b946c90d51b4066eca66b276eefd0bf
Dan <dan2ronen@gmail.com>
2021-01-24 22:56:35 +1300

    App seperation of routes works
    - Added dashboard route and seperated it from the original site's routing.
    NOTE:
    - we can use this for the API routing with the API or run something on the .NET application instead...

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -11,34 +11,55 @@ import Contact from "./routes/contact";
 import Resources from "./routes/resources";
 import Error from "./routes/Error"; // Error page
 import Login from "./routes/login";
-import DashBoard from "./routes/dashBoard";
+import Dashboard from "./routes/dashboard";
 
 const App = () => {
   return (
     <Router>
-      <Navbar></Navbar>
       <Switch>
-        <Route exact path="/">
-          <Home />
+        <Route path="/dashboard">
+          <DashboardApp></DashboardApp>
         </Route>
-        <Route path="/about-me">
-          <About />
-        </Route>
-        <Route path="/resources">
-          <Resources />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        {/** Error site when the url doesn't match any known extensions*/}
-        <Route path="/*">
-          <Error />
+        <Route path="/">
+          <BaseApp></BaseApp>
         </Route>
       </Switch>
     </Router>
+  );
+};
+
+const DashboardApp = () => {
+  return <Dashboard />;
+};
+
+const BaseApp = () => {
+  return (
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about-me">
+            <About />
+          </Route>
+          <Route path="/resources">
+            <Resources />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          {/** Error site when the url doesn't match any known extensions*/}
+          <Route path="/*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 };
 
