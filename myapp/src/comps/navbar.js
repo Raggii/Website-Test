a5d079348fad690be5765b5a1a80477f655ce8d1
SerBABA <dan2ronen@gmail.com>
2021-01-25 18:57:22 +1300

    Started Work on navbar
    - Started with resize changes the menu

--- myapp/src/comps/navbar.js
+++ myapp/src/comps/navbar.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 
+import LogoImage from "../assets/CBS_Logo_20190924_305x194.jpg";
 import styles from "../styles/navbar.module.css";
 
+export const HAMBURGER_WIDTH_THRESHOLD_PX = 921;
+
 function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+  const [isHamburgerMenu, setIsHamburgerMenu] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const updateWidthWindow = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidthWindow);
+    return () => window.removeEventListener("resize", updateWidthWindow);
+  });
+
+  useEffect(() => {
+    setIsHamburgerMenu(width <= HAMBURGER_WIDTH_THRESHOLD_PX);
+  }, [width]);
+
   return (
     <>
       <div className={styles.nav_wrapper}>
@@ -12,7 +32,7 @@ function Navbar() {
             <Link to="/">
               <img
                 className={styles.logo_image}
-                src="http://chasebovineservices.com/wp-content/uploads/2019/09/CBS-Logo.20190924-305x194.jpg"
+                src={LogoImage}
                 alt="Chase Bovine Services"
               />
             </Link>
@@ -20,21 +40,21 @@ function Navbar() {
               Dairy Nutrition and Veterinary Consulting
             </p>
           </div>
-
-          <ul className={styles.nav_links}>
-            <NavbarLinkItem to="/">Home</NavbarLinkItem>
-            <NavbarLinkItem to="/about-me">About me</NavbarLinkItem>
-            <NavbarLinkItem to="/resources">Resources</NavbarLinkItem>
-            <NavbarLinkItem to="/contact">Contact</NavbarLinkItem>
-            <NavbarLinkItem to="/login">Login</NavbarLinkItem>
-          </ul>
+          <div>
+            {isHamburgerMenu ? <h1>MENU</h1> : <></>}
+            <ul className={styles.nav_links}>
+              <NavbarLinkItem to="/">Home</NavbarLinkItem>
+              <NavbarLinkItem to="/about-me">About me</NavbarLinkItem>
+              <NavbarLinkItem to="/resources">Resources</NavbarLinkItem>
+              <NavbarLinkItem to="/contact">Contact</NavbarLinkItem>
+              <NavbarLinkItem to="/login">Login</NavbarLinkItem>
+            </ul>
+          </div>
         </nav>
       </div>
     </>
   );
 }
-
-export default Navbar;
 
 const NavbarLinkItem = ({ to, children }) => {
   return (
@@ -50,3 +70,5 @@ const NavbarLinkItem = ({ to, children }) => {
     </li>
   );
 };
+
+export default Navbar;
