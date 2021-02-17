109130aeae6db7a779085d6be4f308f66c6977cc
vaan3225 <63621952+vaan3225@users.noreply.github.com>
2021-02-17 21:52:09 +1300

    Yes
    Yes

--- myapp/src/comps/Dashboard/DashNav.js
+++ myapp/src/comps/Dashboard/DashNav.js
@@ -4,7 +4,7 @@ import LogoImage from "../../assets/Logo2.png";
 import { NavLink, Link } from "react-router-dom";
 import PageOne from './PageOne';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Styles from '../../styles/dashboardStyles/navBar.module.css'
 
 //Make Button with inputs logo and name
 const DashNav = () => {
@@ -14,15 +14,15 @@ const DashNav = () => {
                 <Link to="/">
                 <img
                     src={LogoImage}
-                    alt="Chase Bovine Services"
+                    alt="Chase Bovine Services "
                 />
                 </Link>
                 
             </div>
             <svg viewBox="0 0 307 500" 
-            xmlns="http://www.w3.org/2000/svg">
+                xmlns="http://www.w3.org/2000/svg">
 
-            <rect width="100" height="100" />
+                <rect className={Styles.box} width='229' height='864'/>
             </svg>
 
         </>
