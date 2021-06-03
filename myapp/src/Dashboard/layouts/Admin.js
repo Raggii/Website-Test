8b9a99c1dee96cbd747311c0c92c682d3712c34b
vaan3225 <63621952+Raggii@users.noreply.github.com>
2021-06-03 23:01:54 +1200

    Try that done!

--- myapp/src/Dashboard/layouts/Admin.js
+++ myapp/src/Dashboard/layouts/Admin.js
@@ -14,7 +14,7 @@
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+* test comment
 */
 import React from "react";
 // javascript plugin used to create scrollbars on windows
