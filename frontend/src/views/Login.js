9134f27b462aef13c99cc76cb228164e2cc40054
SerBABA <32250073+SerBABA@users.noreply.github.com>
2021-06-07 03:06:28 +1200

    Added validation for the password field

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import Api from "../Api";
 
 /**
- * Ensure that a given username confines to the requirements
+ * Ensure that a given username follows the expected requirements before submission
  *
  * @param {string} username User's username
- * @returns {response} Returns the success of the validation and the message associated with it.
+ * @returns {response} The success of the validation and the message associated with it.
  */
 const validateUsername = (username) => {
   const response = { success: true, msg: "" };
@@ -24,10 +24,32 @@ const validateUsername = (username) => {
 };
 
 /**
+ * Ensure that a given password follows the expected requirements before submission
+ *
+ * @param {string} password User's password
+ * @returns {response} The success of the validation and the message associated with it.
+ */
+const validatePassword = (password) => {
+  const response = { success: true, msg: "" };
+
+  if (password.length <= 0) {
+    response.msg = "Password is required.";
+    response.success = false;
+  }
+
+  if (password.length > 30) {
+    response.msg = "Password cannot be longer the 30 characters.";
+    response.success = false;
+  }
+
+  return response;
+};
+
+/**
  * Provides a controlled component functionality for the login form
  * (i.e. keeps track of the username and password for the login form)
  *
- * @returns { values handleChange, handleSubmit } Returns the values as an object and two handlers for
+ * @returns { values handleChange, handleSubmit } The values as an object and two handlers for
  * value changes and submission
  */
 const useLoginForm = () => {
@@ -41,6 +63,27 @@ const useLoginForm = () => {
     password: "",
   });
 
+  const validateFields = () => {
+    const errorsCopy = { ...errors };
+    let success = true;
+
+    // Ensure the username is valid
+    const usernameResponse = validateUsername(values.username);
+    errorsCopy.username = usernameResponse.msg;
+    if (!usernameResponse.success) {
+      success = false;
+    }
+
+    //Ensure that the password is valid
+    const passwordResponse = validatePassword(values.password);
+    errorsCopy.password = passwordResponse.msg;
+    if (!passwordResponse.success) {
+      success = false;
+    }
+
+    return { success, errorsCopy };
+  };
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setValues({
@@ -52,27 +95,25 @@ const useLoginForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Ensure the username is valid
-    const usernameResponse = validateUsername(values.username);
-    if (!usernameResponse.success) {
-      setErrors({
-        ...errors,
-        username: usernameResponse.msg,
-      });
+    const { success, errorsCopy } = validateFields();
+    setErrors(errorsCopy);
+
+    if (!success) {
       return;
     }
 
+    // TODO Implement all cases handling
     Api.signIn(values.username, values.password);
   };
 
-  return { values, handleChange, handleSubmit, errors };
+  return { values, errors, handleChange, handleSubmit };
 };
 
 /**
  * Login form. Contains username and password fields *
  */
 export default function Login() {
-  const { values, handleChange, handleSubmit } = useLoginForm();
+  const { values, errors, handleChange, handleSubmit } = useLoginForm();
 
   return (
     <>
@@ -84,7 +125,7 @@ export default function Login() {
           </div>
         </div>
 
-        <form className="needs-validation px-auto " onSubmit={handleSubmit}>
+        <form className="needs-validation px-auto" noValidate onSubmit={handleSubmit}>
           {/* Username */}
           <div className="row">
             <div className="col">
@@ -94,11 +135,12 @@ export default function Login() {
               <input
                 id="username-input-id"
                 name="username"
-                className="form-control"
+                className={`form-control ${errors.username ? "is-invalid" : ""}`}
                 type="string"
                 onChange={handleChange}
                 value={values.username}
               ></input>
+              {errors.username && <div className="invalid-feedback">{errors.username}</div>}
             </div>
           </div>
 
@@ -111,11 +153,12 @@ export default function Login() {
               <input
                 id="password-input-id"
                 name="password"
-                className="form-control"
+                className={`form-control ${errors.password ? "is-invalid" : ""}`}
                 type="password"
                 onChange={handleChange}
                 value={values.password}
               ></input>
+              {errors.password && <div className="invalid-feedback">{errors.password}</div>}
             </div>
           </div>
 
