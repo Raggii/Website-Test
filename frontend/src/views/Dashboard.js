8d21ff988376aeb9b2ad404b411b6130299fc545
vaan3225 <63621952+Raggii@users.noreply.github.com>
2021-06-11 21:42:50 +1200

    Still having trouble
    YUS

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
 import logoImage from "./Logo2.png";
+import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
+//import {PageOne} from "./PageOne"; -- how to import files
+// import "https://cdn.syncfusion.com/ej2/material.css" ; -- idk why this doesnt work lol
 
 export default function Dashboard() {
   return (
     <div id="wrapper">
         <div id="sidebar">
            <ul className="sidebar-nav">
-              <li><a href="#"> account</a></li>
+              <li><a href="/login"> account</a></li> {/*  this allows for changes between pages*/}
               <li><a href="#"> Page1</a></li>
               <li><a href="#"> pagw2</a></li>
 
            </ul>
         </div>
-
+        {/* this can have the 5 or 6 different pages as individual stuff */}
         <div id="page-content-wrapper">
           <div className="container-fluid">
             <div className="row">
@@ -23,10 +26,11 @@ export default function Dashboard() {
                   d as d as dasdasd wqew qwee qeq weqweq weqwe qweqe qweqwe qw eqwe qweqeqweqwe qweq weqw 
                    qweqw eqwe qwe qwe qweqw eqweqweqewqwe qwe qeq weqw qwe qwe qwe qweqwe qweqw rqw qweqw
                    qweqw eqwe qwe qwe qweqw eqweqweqewqwe qwe qeq weqw qwe qwe qwe qweqwe qweqw rqw qweqw
-                   qweqw eqwe qwe qwe qweqw eqweqweqewqwe qwe qeq weqw qwe qwe qwe qweqwe qweqw rqw qweqw
-                   qweqw eqwe qwe qwe qweqw eqweqweqewqwe qwe qeq weqw qwe qwe qwe qweqwe qweqw rqw qweqw
-                   qweqw eqwe qwe qwe qweqw eqweqweqewqwe qwe qeq weqw qwe qwe qwe qweqwe qweqw rqw qweqw
                 </p>
+                <CalendarComponent></CalendarComponent> {/*ITS A FREAKING CALENDER*/}
+                {/* <PageOne></PageOne> * HOW DO THIS/}
+
+                {/* select a specific DATE  const dateVal = newdate and assign values from this CAN DO REMINDERS */}
               </div>
             </div>
           </div>
---
+++ frontend/src/views/PageOne.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const PageOne = () => {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    Column
+                </div>
+                <div class="col">
+                    Column
+                </div>
+                <div class="col">
+                    Column
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageOne
