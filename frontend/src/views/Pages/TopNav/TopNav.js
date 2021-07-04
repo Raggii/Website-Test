82d642ea1d770a1c7d482cdcf8f0d0b439ca13f9
vaan3225 <63621952+Raggii@users.noreply.github.com>
2021-07-05 00:01:56 +1200

    Update 0.5.11
    FIxed another error

--- frontend/src/views/Pages/TopNav/TopNav.js
+++ frontend/src/views/Pages/TopNav/TopNav.js
@@ -7,9 +7,9 @@ export default function TopNav(props) {
         
         <nav className="navbar">
             <a id="menu-toggle" onClick={() =>props.toggle()}>
-               <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+               <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
             </div>
             </a>
