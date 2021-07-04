7567dda1ed2b5c9b3b903cb961db7433ba217fd6
vaan3225 <63621952+Raggii@users.noreply.github.com>
2021-07-05 01:11:46 +1200

    Update 0.6
    Dan saved my brain

--- frontend/src/views/Pages/TimeBudgetTool/TimeBudgetTool.js
+++ frontend/src/views/Pages/TimeBudgetTool/TimeBudgetTool.js
@@ -1,25 +1,93 @@
 import React from 'react'
 import "./TimeBudgetTool.css"
 import { PieChart } from 'react-minimal-pie-chart';
+import Input from '../../BaseComps/Input';
 
-// <PieChart
-    // data={[
-    //     { title: 'One', value: 10, color: '#E38627', animate:true},
-    //     { title: 'Two', value: 15, color: '#C13C37' },
-    //     { title: 'Three', value: 20, color: '#6A2135' },
-    // ]}
-// />
+<PieChart
+    data={[
+        { title: 'One', value: 10, color: '#E38627', animate:true},
+        { title: 'Two', value: 15, color: '#C13C37' },
+        { title: 'Three', value: 20, color: '#6A2135' },
+    ]}
+/>
 export default function TimeBudgetTool() {
     return (
         <>
         <div className="row text-center">
-            <div className="col-8">
-                <div className="boxContainer">
-                    Pie Chart
+            <div className="col-8 boxContainer ml-3">
+                <div className="pieChart">
+                    <PieChart
+                        data={[
+                            { title: 'One', value: 10, color: '#E38627', animate:true},
+                            { title: 'Two', value: 15, color: '#C13C37' },
+                            { title: 'Three', value: 20, color: '#6A2135' },
+                            { title: 'Three', value: 20, color: '#123456' }
+                        ]}
+                    />
                 </div>
 
             </div>
-            <div className="col-4">Side bar</div>
+            <div className="col">
+                <div className="boxContainer">
+                    <h3>Time Spent</h3>
+                    <div className="row sideBar">
+                        <div className="col-4">Milking</div>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <div className="col-4">Mob Size</div>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <div className="col-4">Miling Platform</div>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">Walking</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">On Feed Pad</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">Drinking</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">Grazing</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">Other</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">Lying</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">Lying</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                    <div className="row sideBar">
+                        <h5 className="col-4">Lying</h5>
+                        <input type="text" class="form-control col-4"/>
+                        <div className="col-4">Hours</div>
+                    </div>
+                </div>
+                
+            </div>
 
         </div>
         </>
--- frontend/src/views/Pages/TopNav/TopNav.js
+++ frontend/src/views/Pages/TopNav/TopNav.js
@@ -8,9 +8,7 @@ export default function TopNav(props) {
         <nav className="navbar">
             <a id="menu-toggle" onClick={() =>props.toggle()}>
                <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
+                <button className="buttonSide"> </button>
             </div>
             </a>
             <ul className="navbar-nav">{props.children}</ul>
