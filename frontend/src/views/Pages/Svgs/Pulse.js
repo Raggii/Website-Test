227acf396ffab6f91d3e043387d609abe940b974
vaan3225 <63621952+Raggii@users.noreply.github.com>
2021-07-04 20:32:24 +1200

    Update 0.4

--- frontend/src/views/Pages/Svgs/Pulse.js
+++ frontend/src/views/Pages/Svgs/Pulse.js
@@ -5,7 +5,7 @@ export default function Pulse({text, colour}) {
     return (
         <div>
             <svg  width="29" height="26" viewBox="0 0 29 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24.8529 13H20.119L16.5686 21.6667L11.8347 4.33334L8.28431 13H3.55042" stroke={colour} stroke-width="1.625" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M24.8529 13H20.119L16.5686 21.6667L11.8347 4.33334L8.28431 13H3.55042" stroke={colour} strokeWidth="1.625" strokeLinecap="round" strokeLinejoin="round"/>
             
             </svg>
             {text}
