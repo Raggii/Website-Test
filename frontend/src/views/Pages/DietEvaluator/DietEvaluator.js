9d781acbadf9d712c83b729cd999cdd36d1342a1
SerBABA <32250073+SerBABA@users.noreply.github.com>
2021-07-06 03:15:18 +1200

    Finished DIet Evaluator

--- frontend/src/views/Pages/DietEvaluator/DietEvaluator.js
+++ frontend/src/views/Pages/DietEvaluator/DietEvaluator.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import BottomTable from './BottomTable'
-import Ingredients from './Ingredients'
-
+import React from "react";
+import BottomTable from "./BottomTable";
+import Ingredients from "./Ingredients";
 
 export default function DietEvaluator() {
-    return (
-        <div className="boxContainer">  
-            <div className="row">
-                <div className="col-4">
-                    <Ingredients></Ingredients>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-2">Space for buttons</div>
-                <div className="col-10"><BottomTable></BottomTable></div>
-            </div>
+  return (
+    <div>
+      <div className="boxContainer">
+        <Ingredients></Ingredients>
+      </div>
+      <div className="boxContainer" style={{ marginTop: "3rem" }}>
+        <div className="row">
+          <div className="col-2">Space for buttons</div>
+          <div className="col-10">
+            <BottomTable></BottomTable>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
--- frontend/src/views/Pages/DietEvaluator/Ingredients.js
+++ frontend/src/views/Pages/DietEvaluator/Ingredients.js
@@ -1,20 +1,97 @@
-import React from 'react'
-import DropBox from '../../BaseComps/DropBox'
+import React from "react";
+import styles from "./Ingrediants.module.css";
+
+const IngediantRow = () => {
+  return (
+    <div className={styles.dataRow}>
+      <select className={styles.ingrediant}>
+        <option value={0}>Spring - Irrigated</option>
+      </select>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+      <div className={styles.dataCol}>
+        <input />
+      </div>
+    </div>
+  );
+};
 
 export default function Ingredients() {
-    return (
-        <div className="">
-            <h4>Ingredient</h4>
-            <DropBox className="m-1"></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-            <DropBox></DropBox>
-        </div>
-    )
+  return (
+    <div className={styles.tableGrid}>
+      <div className={styles.headerRow}>
+        <div className={styles.ingrediant}>Ingrediant</div>
+        <div className={styles.dataCol}>Kgs</div>
+        <div className={styles.dataCol}>Wet/Dry</div>
+        <div className={styles.dataCol}>Util %</div>
+        <div className={styles.dataCol}>DMI</div>
+        <div className={styles.dataCol}>DM%</div>
+        <div className={styles.dataCol}>Site</div>
+
+        <div className={styles.dataCol}>ME</div>
+        <div className={styles.dataCol}>CP</div>
+        <div className={styles.dataCol}>NDF</div>
+        <div className={styles.dataCol}>Ca</div>
+        <div className={styles.dataCol}>P</div>
+        <div className={styles.dataCol}>Mg</div>
+        <div className={styles.dataCol}>K</div>
+        <div className={styles.dataCol}>Na</div>
+        <div className={styles.dataCol}>CI</div>
+        <div className={styles.dataCol}>S</div>
+      </div>
+
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+      <IngediantRow></IngediantRow>
+    </div>
+  );
 }
