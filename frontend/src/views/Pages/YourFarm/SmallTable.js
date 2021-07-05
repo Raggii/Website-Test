e201ace88843b1b8cb246c2a1b1931f9e4450d70
vaan3225 <63621952+Raggii@users.noreply.github.com>
2021-07-05 19:46:20 +1200

    For dan

--- frontend/src/views/Pages/YourFarm/SmallTable.js
+++ frontend/src/views/Pages/YourFarm/SmallTable.js
@@ -4,8 +4,8 @@ function Controller({Title, Amount}) {
     return (
         <div>
             <tr>
-                <th scope="row">3</th>
-      <td colspan="2" class="table-active">Larry the Bird</td>
+                <th colspan="2">3</th>
+                <td colspan="2" class="table-active">Larry the Bird</td>
             </tr>
         </div>
 
@@ -20,7 +20,18 @@ export default function SmallTable() {
 
                 <h5 className=" d-flex justify-content-center">Supplemental Intake</h5>
                 <table class="table">
-                <Controller Title = "total Zinc mg/day" Amount="123"></Controller>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <Controller Title = "total Zinc mg/day" Amount="123"></Controller>
+                    </tbody>
                 </table>
 
             </div>
