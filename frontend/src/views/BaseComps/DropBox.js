ee3da858f468bb3235c2d17c0ae25c132bfb11ab
SerBABA <32250073+SerBABA@users.noreply.github.com>
2021-07-06 02:14:18 +1200

    Third collect page is working

--- frontend/src/views/BaseComps/DropBox.js
+++ frontend/src/views/BaseComps/DropBox.js
@@ -1,41 +1,51 @@
-import React from 'react'
+import React from "react";
 
 //import  "bootstrap.bundle.min.js"
 
-export default function DropBox({title,opt1,opt2,opt3,Error}) {
-    return (
-        <>
-            <div>
-                {title ? <span>{title}</span> : null}
-                <div>
-
-                    <select  style={{width: "100%",borderColor: "#ced4da",padding: "7px"}}>
-                        <option value="">Select an Option</option>
-                        <option value="Male">{opt1}</option>
-                        <option value="Female">{opt2}</option>
-                        <option value="Other">{opt3}</option>
-                    </select>
-                    <div className="dropdown-menu">
-                        <div class="inner show">
-                            <ul className="dropdown-menu inner show">
-                                <li><a className="dropdown-item">
-                                    <span >{opt1}</span></a>
-                                </li>
-                                <li><a className="dropdown-item" >
-                                    <span >{opt2}</span></a>
-                                </li>
-                                <li><a className="dropdown-item" >
-                                    <span >{opt3}</span></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                {/* If Error Comes up display it below */}
-                {Error ? <label></label> : null}
+export default function DropBox({ title, opt1, opt2, opt3, Error, opt4 }) {
+  return (
+    <>
+      <div>
+        {title ? <span>{title}</span> : null}
+        <div>
+          <select style={{ width: "100%", borderColor: "#ced4da", padding: "7px" }}>
+            <option value="">Select an Option</option>
+            <option value="Male">{opt1}</option>
+            <option value="Female">{opt2}</option>
+            <option value="Other">{opt3}</option>
+          </select>
+          <div className="dropdown-menu">
+            <div class="inner show">
+              <ul className="dropdown-menu inner show">
+                <li>
+                  <a className="dropdown-item">
+                    <span>{opt1}</span>
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item">
+                    <span>{opt2}</span>
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item">
+                    <span>{opt3}</span>
+                  </a>
+                </li>
+                {opt4 ? (
+                  <li>
+                    <a className="dropdown-item">
+                      <span>{opt3}</span>
+                    </a>
+                  </li>
+                ) : null}
+              </ul>
             </div>
-
-
-        </>    
-        )
+          </div>
+        </div>
+        {/* If Error Comes up display it below */}
+        {Error ? <label></label> : null}
+      </div>
+    </>
+  );
 }
--- frontend/src/views/InitialInputs/ThirdPage.js
+++ frontend/src/views/InitialInputs/ThirdPage.js
@@ -1,81 +1,73 @@
-import React from 'react'
-import RadioButton from "../BaseComps/RadioButton"
-import { FormWrapper } from '../../comps/FromWrapper/FormWrapper'
+import React, { useState, useEffect } from "react";
+import styles from "./ThridPage.module.css";
+import { FormWrapper } from "../../comps/FromWrapper/FormWrapper";
 
+function LevelSelection({ title }) {
+  const [option, setOption] = useState(0);
+
+  const handleChange = (event) => {
+    setOption(event.target.value);
+  };
+
+  return (
+    <div className={styles.selectionWrap}>
+      <h6 className={styles.selectTitle}>{title}</h6>
+      <select className={styles.selectLevel} value={option} onChange={handleChange}>
+        <option value={0}>None</option>
+        <option value={1}>Slight</option>
+        <option value={2}>Moderate</option>
+        <option value={3}>Very</option>
+      </select>
+    </div>
+  );
+}
 
 export default function ThirdPage() {
-    return (
-        <FormWrapper>  
+  return (
+    <FormWrapper>
+      <div className="d-flex justify-content-center mt-5">
+        <div className="loginContainerRadio">
+          <h2 className="m-5 text-center">Animal Health Concerns</h2>
+          <div className={styles.animalHealthConcernWrap}>
+            <LevelSelection title="Lameness"></LevelSelection>
+            <LevelSelection title="Somatic Cell Count"></LevelSelection>
+            <LevelSelection title="Body Condition"></LevelSelection>
 
-            <div className="d-flex justify-content-center mt-5">
-                
-                <div className="loginContainerRadio">
-                    
-                    <h2 className="m-5 text-center">
-                        Animal Health Concerns
-                    </h2>
-                    <span className="inline-flex text-left">
+            <h5 className={styles.groupTitle}>Reproductive Performance</h5>
+            <LevelSelection title="NonCycler Cows"></LevelSelection>
+            <LevelSelection title="6 weeks in calf (%)"></LevelSelection>
+            <LevelSelection title="Final Empty (%)"></LevelSelection>
 
-                        <div className="row mb-2"> 
-                            <div className= "col-4 "></div>
-                            <div className= "col-2">None</div>
-                            <div className= "col-2">Slight</div>
-                            <div className= "col-2">Moderate</div>
-                            <div className= "col-2">Very</div>
-                        </div>
-                        <div className="row">
-                            <RadioButton title = "Lameness"></RadioButton>
-                            <RadioButton title = "Somatic Cell Count"></RadioButton>
-                            <RadioButton title = "Body Condition"></RadioButton>
-                        </div>
-                        <div className ="mt-2 mb-1">
-                            <h6>Reproductive Preformance</h6>
-                        </div>
-                        <div className="row">
-                            <RadioButton title = "NonCyler Cows"></RadioButton>
-                            <RadioButton title = "6wk inCalf %"></RadioButton>
-                            <RadioButton title = "Final Empty"></RadioButton>
-                        </div>
-                        <div className ="mt-2 mb-1">
-                            <h6 >Transition Disease Incidence</h6>
-                        </div>
-                        <div className="row">
-                            <RadioButton title = "Uterine Disease"></RadioButton>
-                            <RadioButton title = "Mastitis"></RadioButton>
-                            <RadioButton title = "Ketosis Metabolic"></RadioButton>
-                        </div>
-                        <div className ="mt-2 mb-1">
-                            <h6 >Trace Mineral Monitoring</h6>
-                        </div>
-                        <div className="row">
-                            <RadioButton title = "Selenium Status"></RadioButton>
-                            <RadioButton title = "Copper Deficiency"></RadioButton>
-                            <RadioButton title = "Copper Toxicity"></RadioButton>
-                        </div>
-                        <div className ="mt-2 mb-1">
-                            <h6 >Dietary Antagonists</h6>
-                        </div>
-                        <div className="row">
-                            <RadioButton title = "Iron"></RadioButton>
-                            <RadioButton title = "Molydenum"></RadioButton>
-                        </div>
+            <h5 className={styles.groupTitle}>Transition Disease Incidence</h5>
+            <LevelSelection title="Uterine Disease - RFM"></LevelSelection>
+            <LevelSelection title="Mastities"></LevelSelection>
+            <LevelSelection title="Ketosis - Metabolic Dz"></LevelSelection>
 
-                        <div className="buttons mt-4">
-                            <div className="row">
-                                <div className="col-6">
-                                    <a type="button" class="btn btn-danger">Cancel</a>
-                                </div>
-                                <div className="col-6">
-                                    <a type="button" class="btn btn-primary" href="/DataCollect4">Continue</a>
-                                </div>
-                            </div>
-                        </div>
-                    </span>
-                </div>
-                
+            <h5 className={styles.groupTitle}>Trace Miniral Monitoring</h5>
+            <LevelSelection title="Selenium Status"></LevelSelection>
+            <LevelSelection title="Copper Deficiency"></LevelSelection>
+            <LevelSelection title="Copper Toxcitiy"></LevelSelection>
+
+            <h5 className={styles.groupTitle}>Dietary Antagonists</h5>
+            <LevelSelection title="Iron"></LevelSelection>
+            <LevelSelection title="Molybdenum"></LevelSelection>
+          </div>
+          <div className="buttons mt-4">
+            <div className="row">
+              <div className="col-6">
+                <a type="button" class="btn btn-danger">
+                  Cancel
+                </a>
+              </div>
+              <div className="col-6">
+                <a type="button" class="btn btn-primary" href="/DataCollect4">
+                  Continue
+                </a>
+              </div>
             </div>
-
-            
-        </FormWrapper>
-    )
+          </div>
+        </div>
+      </div>
+    </FormWrapper>
+  );
 }
