c270a3e2f4056d37a2a3704aeb1aaa78341ae1ff
SerBABA <32250073+SerBABA@users.noreply.github.com>
2021-07-05 19:31:48 +1200

    Reminders added

--- frontend/src/comps/DashboardCalender/DashboardCalender.js
+++ frontend/src/comps/DashboardCalender/DashboardCalender.js
@@ -1,22 +1,75 @@
 import { CalendarComponent } from "@syncfusion/ej2-react-calendars";
+import { useEffect, useState } from "react";
+import ReminderIcon from "../../views/Pages/Svgs/ReminderHolder";
+import ClockIcon from "../../views/Pages/Svgs/Clock";
 import styles from "./DashboardCalender.module.css";
+import { format } from "date-fns";
+
+const Reminder = ({ date, text, prioity }) => {
+  const [fill, setFill] = useState("");
+  const [color, setColor] = useState("none");
+
+  function addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+  function getPriorityColour(priority) {
+    switch (prioity) {
+      case 0:
+        return "red";
+      case 1:
+        return "orange";
+      case 2:
+        return "lightgreen";
+      default:
+        return "grey";
+    }
+  }
+
+  useEffect(async () => {
+    setFill(getPriorityColour(prioity));
+  }, [prioity, date]);
+
+  return (
+    <li>
+      <ReminderIcon fill={fill} color={color} className={styles.reminderIcon} />
+      <div className={styles.reminderCenterWrap}>
+        <div className={styles.reminderTitle}>
+          {text.length > 15 ? text.slice(0, 12) + "..." : text.slice(0, 15)}
+        </div>
+        <div className={styles.date}>{format(date, "do MMMM u")}</div>
+      </div>
+      <div className={styles.hours}>
+        <ClockIcon />
+        <div>
+          {addZero(date.getHours())}:{addZero(date.getMinutes())}
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export const DashboardCalender = () => {
   return (
     <div className={styles.calenderWrap}>
-      <CalendarComponent color={"#00f"}></CalendarComponent>
-      {/* 
-        const dateValue: Date = new Date(New Date().getFullYear(), new Date().getMonth, 20)
-        assign to value 
-        isMultiSelection ={true}
-                    */}
-      <h6>Reminders</h6>
-      <ul>
-        <li>First</li>
-        <li>Second</li>
-        <li>Third</li>
-        <li>Forth</li>
-      </ul>
+      <CalendarComponent
+        color={"#00f"}
+        className={styles.calender}
+        style={{ borderRadius: "0.5rem" }}
+      />
+
+      <div className={styles.reminderWrap}>
+        <h5 className="fw-bold">REMINDERS</h5>
+        <ul className={styles.reminderList}>
+          <Reminder text="Payment One" date={new Date()} prioity={0} />
+          <Reminder text="Soil Free" date={new Date()} prioity={1} />
+          <Reminder text="Upgrade plan page" date={new Date()} prioity={1} />
+          <Reminder text="Payment Two" date={new Date()} prioity={2} />
+        </ul>
+      </div>
     </div>
   );
 };
--- frontend/src/views/Pages/Svgs/ReminderHolder.js
+++ frontend/src/views/Pages/Svgs/ReminderHolder.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from "react";
 
-export default function ReminderHolder() {
-    return (
-        <div>
-            <svg width="57" height="59" viewBox="0 0 57 59" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect y="-2.17754" width="43.0915" height="43.0915" rx="13.5" transform="matrix(0.687856 0.725848 -0.687856 0.725848 27.0022 -0.0795225)" fill="#90C335" stroke="ffffff" strokeWidth="3"/>
-            <mask id="mask0" maskType="alpha" maskUnits="userSpaceOnUse" x="2" y="2" width="53" height="55">
-            <rect y="-2.17754" width="43.0915" height="43.0915" rx="13.5" transform="matrix(0.687856 0.725848 -0.687856 0.725848 27.0022 -0.0795225)" fill="white" stroke="ffffff" strokeWidth="3"/>
-            </mask>
-            <g mask="url(#mask0)">
-            </g>
-            </svg>
-
-        </div>
-    )
+export default function ReminderHolder({ color, fill, className }) {
+  return (
+    <div className={className}>
+      <svg
+        width="57"
+        height="59"
+        viewBox="0 0 57 59"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ maxWidth: "40px" }}
+      >
+        <rect
+          y="-2.17754"
+          width="43.0915"
+          height="43.0915"
+          rx="13.5"
+          transform="matrix(0.687856 0.725848 -0.687856 0.725848 27.0022 -0.0795225)"
+          fill={fill}
+          stroke={color}
+          strokeWidth="3"
+        />
+        <mask
+          id="mask0"
+          masktype="alpha"
+          maskUnits="userSpaceOnUse"
+          x="2"
+          y="2"
+          width="53"
+          height="55"
+        >
+          <rect
+            y="-2.17754"
+            width="43.0915"
+            height="43.0915"
+            rx="13.5"
+            transform="matrix(0.687856 0.725848 -0.687856 0.725848 27.0022 -0.0795225)"
+            fill="fff"
+            stroke="ffffff"
+            strokeWidth="3"
+          />
+        </mask>
+        <g mask="url(#mask0)"></g>
+      </svg>
+    </div>
+  );
 }
