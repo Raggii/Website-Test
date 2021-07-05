40965dfeede6b7717a0af99d035999a73f50920a
SerBABA <32250073+SerBABA@users.noreply.github.com>
2021-07-05 19:48:35 +1200

    UES

--- frontend/src/views/Pages/YourFarm/SmallTable.js
+++ frontend/src/views/Pages/YourFarm/SmallTable.js
@@ -1,48 +1,41 @@
-import React from 'react'
+import React from "react";
 
-function Controller({Title, Amount}) {
-    return (
-        <div>
-            <tr>
-                <th colspan="2">3</th>
-                <td colspan="2" class="table-active">Larry the Bird</td>
-            </tr>
-        </div>
-
-    )
+function Controller({ Title, Amount }) {
+  return (
+    <tr>
+      <td colspan="2">3</td>
+      <td colspan="2" class="table-active">
+        Larry the Bird
+      </td>
+    </tr>
+  );
 }
 
-
 export default function SmallTable() {
-    return (
-        <div>
-            <div className="boxContainer m-2">
-
-                <h5 className=" d-flex justify-content-center">Supplemental Intake</h5>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <Controller Title = "total Zinc mg/day" Amount="123"></Controller>
-                    </tbody>
-                </table>
-
-            </div>
-            <div className="boxContainer m-2">
-
-                <h5 className=" d-flex justify-content-center">Supplemental Total Dietary Concentraion</h5>
-            </div>
-            <div className="boxContainer m-2">
-
-                <h5 className=" d-flex justify-content-center">Dietary Concentrations</h5>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="boxContainer m-2">
+        <h5 className=" d-flex justify-content-center">Supplemental Intake</h5>
+        <table class="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <Controller Title="total Zinc mg/day" Amount="123"></Controller>
+          </tbody>
+        </table>
+      </div>
+      <div className="boxContainer m-2">
+        <h5 className=" d-flex justify-content-center">Supplemental Total Dietary Concentraion</h5>
+      </div>
+      <div className="boxContainer m-2">
+        <h5 className=" d-flex justify-content-center">Dietary Concentrations</h5>
+      </div>
+    </div>
+  );
 }
